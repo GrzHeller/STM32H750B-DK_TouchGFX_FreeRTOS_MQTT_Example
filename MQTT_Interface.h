31a6c65716dc355520267d5059bd41b4dd0aec31
GrzHeller <73969883+GrzHeller@users.noreply.github.com>
2021-12-17 22:22:04 +0100

    Delete MQTT_Interface.h

--- MQTT_Interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MQTT_INTERFACE_H_
-#define _MQTT_INTERFACE_H_
-#include <mqtt.h>
-
-void interface_mqtt_connect(mqtt_client_t *client, const struct mqtt_connect_client_info_t *ci, void *arg);
-void interface_mqtt_publish(mqtt_client_t *client, void *arg);
-
-#endif // _MQTT_INTERFACE_H_
